0a73d7d009335ed86ebbd6bf31ce33180182f906
Maria Livia Chiorean <maria-livia.chiorean@guardian.co.uk>
2016-02-10 18:07:17 +0000

    Initial Commit

---
+++ Hiking.v2/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Hiking.v2
+//
+//  Created by mliviachiorean on 10/02/2016.
+//  Copyright © 2016 mliviachiorean. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
